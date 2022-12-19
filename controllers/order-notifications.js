fe797eaeccfabfc8f1bee1e363507188114363a6
Hitoishi Das <hitoishi.d@technovert.com>
2022-12-19 18:33:21 +0530

    Added 201 to post

--- controllers/order-notifications.js
+++ controllers/order-notifications.js
@@ -3,6 +3,8 @@ const orderNotificationMethods = {};
 orderNotificationMethods.sendOrderNotification = (req, res, next) => {
   console.log(req.headers);
   console.log(req.body);
+
+  return res.status(201);
 };
 
 module.exports = orderNotificationMethods;
